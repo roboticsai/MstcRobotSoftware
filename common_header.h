483d774a393eb41dce92d58701dbf7b686085b7b
dinesh lama <dinesh99981@gmail.com>
2020-02-20 18:52:31 +0545

    latest change

--- common_header.h
+++ common_header.h
@@ -1,48 +1,103 @@
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 #include <iostream>
+#include "SFML/Graphics.hpp"
+#include <unordered_map>
+#include <iostream>
+#include <list>
 using namespace std;
 
 enum Key {
-  null, eforward, ebackward, eleft, eright, inc_speed, dec_speed, stop
+  null = -1, eforward = 0, ebackward=115, eleft=97, eright=100, inc_speed=101, dec_speed=113
 };
 
 enum MouseBut {
-    None, RightBut, LeftBut, MidBut
-};
-
-struct MousePos {
-    int x = -1;
-    int y = -1;
+    None, RightBut, LeftBut, MidBut, MidButScroll
 };
 
 struct Mouse {
-    MousePos mMousePos;
+    sf::Vector2i mMousePos;
     MouseBut mouseBut;
     double MidleButScrollPos = 0.0;
 } mouse;
 
 class UserInput {
 public:
-    UserInput() {};
+    UserInput();
     ~UserInput() {};
-    Key mKey;
+    void GetUserInput(sf::RenderWindow &renderWindow,sf::Event event);
+    std::vector<int> mKeys;
     Mouse mMouse;
+    std::unordered_map<int, bool> keys;
+    std::list<int> changedKeys;
+    void DisPlayValues();
     friend class boost::serialization::access;
     template<class Archive>
     void serialize(Archive & ar, const unsigned int version)
     {
-        ar & mKey;
+        ar & mKeys;
         ar & mMouse;
     }
-} userInput;
+};
 
 class RobotData {
 public:
 
 };
 
-std::ostream& operator<<(std::ostream &os, const UserInput &userInput) {
-    os<<"{{Key:"<<userInput.mKey<<"},{Mouse Pos :("<<userInput.mMouse.mMousePos.x<<","<<userInput.mMouse.mMousePos.y<<"),MouseBut Clked : "<<userInput.mMouse.mouseBut<<",Scroll pos : "<<userInput.mMouse.MidleButScrollPos<<"}}"<<endl;
-    return  os;
+void UserInput::DisPlayValues() {
+  if(mKeys.empty())
+    std::cout<<"{{Key:"<<null<<"},{Mouse Pos :("<<mMouse.mMousePos.x<<","<<mMouse.mMousePos.y<<"),MouseBut Clked : "<<mMouse.mouseBut<<",Scroll pos : "<<mMouse.MidleButScrollPos<<"}}"<<std::endl;
+  else if(mKeys.size() == 1) {
+    std::cout<<"{{Key:"<<mKeys[0]<<"},{Mouse Pos :("<<mMouse.mMousePos.x<<","<<mMouse.mMousePos.y<<"),MouseBut Clked : "<<mMouse.mouseBut<<",Scroll pos : "<<mMouse.MidleButScrollPos<<"}}"<<endl;
+  }
+  else if(mKeys.size() == 2) {
+    std::cout<<"{{Key:"<<mKeys[0]<<","<<mKeys[1]<<"},{Mouse Pos :("<<mMouse.mMousePos.x<<","<<mMouse.mMousePos.y<<"),MouseBut Clked : "<<mMouse.mouseBut<<",Scroll pos : "<<mMouse.MidleButScrollPos<<"}}"<<endl;
+  }
+  else if(mKeys.size() == 3) {
+    std::cout<<"{{Key:"<<mKeys[0]<<","<<mKeys[1]<<","<<mKeys[1]<<"},{Mouse Pos :("<<mMouse.mMousePos.x<<","<<mMouse.mMousePos.y<<"),MouseBut Clked : "<<mMouse.mouseBut<<",Scroll pos : "<<mMouse.MidleButScrollPos<<"}}"<<endl;
+  }
+}
+
+UserInput::UserInput() {
+  std::cout<<"construtor called"<<std::endl;
+}
+
+void UserInput::GetUserInput(sf::RenderWindow &renderWindow,sf::Event event) {
+  if (event.type == sf::Event::EventType::Closed)
+    renderWindow.close();
+
+  if (event.type == sf::Event::EventType::KeyPressed){
+    if (keys.count(event.key.code) == 0){
+      keys[event.key.code] = true;
+      changedKeys.push_back(event.key.code);
+    }
+  }
+  if (event.type == sf::Event::EventType::KeyReleased){
+    if (keys.count(event.key.code) == 1){
+      keys.erase(event.key.code);
+      changedKeys.push_back(event.key.code);
+    }
+  }
+  mKeys.clear();
+  for (auto& keyValue : keys)
+    mKeys.push_back(keyValue.first);
+
+  sf::Vector2i localPosition = sf::Mouse::getPosition(renderWindow);
+  mMouse.mouseBut = None;
+  if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+    mMouse.mouseBut = LeftBut;
+  }
+  else if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+    mMouse.mouseBut = RightBut;
+  }
+  else if(sf::Mouse::isButtonPressed(sf::Mouse::Middle)) {
+    mMouse.mouseBut = MidBut;
+  }
+  else if(event.type == sf::Event::MouseWheelScrolled) {
+    mMouse.MidleButScrollPos+=event.mouseWheelScroll.delta;
+  }
+  if(event.type == sf::Event::MouseMoved) {
+    mMouse.mMousePos = localPosition;
+  }
 }
--- user_input.cpp
+++ user_input.cpp
@@ -4,6 +4,8 @@
 #include <unordered_map>
 #include <iostream>
 #include <list>
+#include "common_header.h"
+
 int main(int argc, char ** argv) {
   sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "SFML Demo");
 
@@ -12,59 +14,11 @@ int main(int argc, char ** argv) {
   // If true, you will continue to receive keyboard events when a key is held down
   // If false, it will only fire one event per press until released
   renderWindow.setKeyRepeatEnabled(false);
-
-  std::unordered_map<int, bool> keys;
-  std::list<int> changedKeys;
-  int count = 0;
+  UserInput user_input;
   while (renderWindow.isOpen()){
-    changedKeys.clear();
-
     while (renderWindow.pollEvent(event)){
-      std::cout<<"in-------------"<<count++<<std::endl;
-
-      if (event.type == sf::Event::EventType::Closed)
-        renderWindow.close();
-
-      if (event.type == sf::Event::EventType::KeyPressed){
-        if (keys.count(event.key.code) == 0){
-          keys[event.key.code] = true;
-          changedKeys.push_back(event.key.code);
-        }
-      }
-      if (event.type == sf::Event::EventType::KeyReleased){
-        if (keys.count(event.key.code) == 1){
-          keys.erase(event.key.code);
-          changedKeys.push_back(event.key.code);
-        }
-      }
-      sf::Vector2i localPosition = sf::Mouse::getPosition(renderWindow);
-      if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-        std::cout<<"Left mouse wheel clicked at pos ="<<localPosition.x<<"\t"<<localPosition.y<<std::endl;
-      }
-      else if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
-        std::cout<<"Right mouse wheel clicked at pos ="<<localPosition.x<<"\t"<<localPosition.y<<std::endl;
-      }
-      else if(sf::Mouse::isButtonPressed(sf::Mouse::Middle)) {
-        std::cout<<"Middle mouse wheel clicked at pos ="<<localPosition.x<<"\t"<<localPosition.y<<std::endl;
-      }
-      else if(event.type == sf::Event::MouseWheelScrolled) {
-        std::cout << "mouse x: " << event.mouseWheelScroll.x << std::endl;
-        std::cout << "mouse y: " << event.mouseWheelScroll.y << std::endl;
-      }
-      if(event.type == sf::Event::MouseMoved) {
-        std::cout<<"mouse pos="<<localPosition.x<<"\t"<<localPosition.y<<std::endl;
-      }
-    }
-
-    if(!keys.empty()) {
-      std::cout << "Currently pressed keys: ";
-      // To get the actual value as a string, you need to use Thor or write your own version
-      for (auto& keyValue : keys)
-        std::cout << keyValue.first << " ";
-      std::cout << std::endl;
-    }
-    if (!changedKeys.empty()){
-      std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Keys changed" << std::endl;
+      user_input.GetUserInput(renderWindow,event);
+      user_input.DisPlayValues();
     }
     renderWindow.clear();
     renderWindow.display();

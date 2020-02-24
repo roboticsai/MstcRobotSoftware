277d839e6027f43f2232db73a23e0a8e604de4d9
dinesh999lama <dinesh99981@gmail.com>
2020-02-24 13:48:24 +0545

    name change

---
+++ jaguar_s1_controller.cpp
@@ -0,0 +1,26 @@
+#include "socket_comm.h"
+
+int main(int argc, char *argv[])
+{
+  Controller controller;
+  UserInput user_input;
+  RobotData robot_data;
+  SfImg sfImg(controller.newsockfd);
+
+   sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "Remote Computer");
+   sf::Event event;
+   renderWindow.setKeyRepeatEnabled(false);
+
+   while (renderWindow.isOpen()){
+     auto sprite = sfImg.GetSfImg();
+     while (renderWindow.pollEvent(event)){
+         GetUserInput(renderWindow,event,user_input);
+     }
+     SendUserInput(controller.newsockfd,user_input);
+     ReadRobotInfo(controller.newsockfd,robot_data);
+     robot_data.Display();
+     renderWindow.draw(sprite);
+     renderWindow.display();
+   }
+   return 0;
+}

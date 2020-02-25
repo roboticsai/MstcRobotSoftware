2a846ecc40b1e59514f94320729d99b744b3ec37
gkesh <gkeshthapa@gmail.com>
2020-02-25 11:58:48 +0545

    Socket code updated with static IP for testing

--- client.cpp
+++ client.cpp
@@ -33,11 +33,9 @@ int main(int argc, char *argv[])
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    serv_addr.sin_addr.s_addr = inet_addr(server);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
